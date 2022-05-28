e3e66f0ca5461cc68f6bdc272236b3d9344a38b9
rob-essex <94251978+rob-essex@users.noreply.github.com>
2022-05-28 16:36:19 -0400

    correct file
    py file showed html

--- system_implementation.py
+++ system_implementation.py
@@ -1,272 +1,136 @@
-<!DOCTYPE HTML>
-<html>
 
-<head>
-    <meta charset="utf-8">
+class Patient:
+    """Creates a Patient object with a patient's name, address, phone number."""
+    def __init__(self, name, address, phone):
+        self.name = name
+        self.address = address
+        self.phone = phone
 
-    <title>system_implementation.py (editing)</title>
-    <link id="favicon" rel="shortcut icon" type="image/x-icon" href="/static/base/images/favicon-file.ico?v=e2776a7f45692c839d6eea7d7ff6f3b2">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=3c2a865c832a1322285c55c6ed99abb2" type="text/css" />
-    <link rel="stylesheet" href="/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=9df10041c3e07da766e7c48dd4c35e4a" type="text/css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    
-<link rel="stylesheet" href="/static/components/codemirror/lib/codemirror.css?v=fc217d502b05f65616356459c0ec1d62">
-<link rel="stylesheet" href="/static/components/codemirror/addon/dialog/dialog.css?v=c89dce10b44d2882a024e7befc2b63f5">
+    def request_repeat(self, prescription):
+        """Accepts a Prescription object as an argument, verifies it belongs to Patient, and sends for repeat."""
+        if prescription.patient == self.name:
+            # Print operation serves as a placeholder since surgery request business process is unknown.
+            print("Sending repeat prescription request of ", prescription.prescription_type, " to ",
+                  prescription.doctor, ".")
+        else:
+            print("This prescription does not belong to this patient.")
 
-    <link rel="stylesheet" href="/static/style/style.min.css?v=2165fc0d023f0baf5cce3b2a6db40e22" type="text/css"/>
-    
-
-    <link rel="stylesheet" href="/custom/custom.css" type="text/css" />
-    <script src="/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
-    <script src="/static/components/react/react.production.min.js?v=34f96ffc962a7deecc83037ccb582b58" type="text/javascript"></script>
-    <script src="/static/components/react/react-dom.production.min.js?v=b14d91fb641317cda38dbc9dbf985ab4" type="text/javascript"></script>
-    <script src="/static/components/create-react-class/index.js?v=94feb9971ce6d26211729abc43f96cd2" type="text/javascript"></script>
-    <script src="/static/components/requirejs/require.js?v=951f856e81496aaeec2e71a1c2c0d51f" type="text/javascript" charset="utf-8"></script>
-    <script>
-      require.config({
-          
-          urlArgs: "v=20220523142421",
-          
-          baseUrl: '/static/',
-          paths: {
-            'auth/js/main': 'auth/js/main.min',
-            custom : '/custom',
-            nbextensions : '/nbextensions',
-            kernelspecs : '/kernelspecs',
-            underscore : 'components/underscore/underscore-min',
-            backbone : 'components/backbone/backbone-min',
-            jed: 'components/jed/jed',
-            jquery: 'components/jquery/jquery.min',
-            json: 'components/requirejs-plugins/src/json',
-            text: 'components/requirejs-text/text',
-            bootstrap: 'components/bootstrap/dist/js/bootstrap.min',
-            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
-            'jquery-ui': 'components/jquery-ui/jquery-ui.min',
-            moment: 'components/moment/min/moment-with-locales',
-            codemirror: 'components/codemirror',
-            termjs: 'components/xterm.js/xterm',
-            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead.min',
-          },
-          map: { // for backward compatibility
-              "*": {
-                  "jqueryui": "jquery-ui",
-              }
-          },
-          shim: {
-            typeahead: {
-              deps: ["jquery"],
-              exports: "typeahead"
-            },
-            underscore: {
-              exports: '_'
-            },
-            backbone: {
-              deps: ["underscore", "jquery"],
-              exports: "Backbone"
-            },
-            bootstrap: {
-              deps: ["jquery"],
-              exports: "bootstrap"
-            },
-            bootstraptour: {
-              deps: ["bootstrap"],
-              exports: "Tour"
-            },
-            "jquery-ui": {
-              deps: ["jquery"],
-              exports: "$"
-            }
-          },
-          waitSeconds: 30,
-      });
-
-      require.config({
-          map: {
-              '*':{
-                'contents': 'services/contents',
-              }
-          }
-      });
-
-      // error-catching custom.js shim.
-      define("custom", function (require, exports, module) {
-          try {
-              var custom = require('custom/custom');
-              console.debug('loaded custom.js');
-              return custom;
-          } catch (e) {
-              console.error("error loading custom.js", e);
-              return {};
-          }
-      })
-
-    document.nbjs_translations = {"domain": "nbjs", "locale_data": {"nbjs": {"": {"domain": "nbjs"}}}};
-    document.documentElement.lang = navigator.language.toLowerCase();
-    </script>
-
-    
-    
-
-</head>
-
-<body class="edit_app "
- 
-data-base-url="/"
-data-file-path="system_implementation.py"
-
-  
- 
-
-dir="ltr">
-
-<noscript>
-    <div id='noscript'>
-      Jupyter Notebook requires JavaScript.<br>
-      Please enable it to proceed. 
-  </div>
-</noscript>
-
-<div id="header" role="navigation" aria-label="Top Menu">
-  <div id="header-container" class="container">
-  <div id="ipython_notebook" class="nav navbar-brand"><a href="/tree" title='dashboard'>
-      <img src='/static/base/images/logo.png?v=641991992878ee24c6f3826e81054a0f' alt='Jupyter Notebook'/>
-  </a></div>
-
-  
-
-<span id="save_widget" class="pull-left save_widget">
-    <span class="filename"></span>
-    <span class="last_modified"></span>
-</span>
+    def request_appointment(self, appointment_type, appointment_staff):
+        """Generates a request message from the Patient with specified appointment type and staff."""
+        # Print operation serves as a placeholder since surgery request business process is unknown.
+        print(self.name, "requests a(n) ", appointment_type, "appointment with ", appointment_staff.name, ".")
 
 
-  
+class HealthcareProfessional:
+    """Creates a Healthcare Professional object with a name and employee number."""
+    def __init__(self, name, employee_number):
+        self.name = name
+        self.employee_number = employee_number
 
-  
-  
-  
-  
-
-    <span id="login_widget">
-      
-    </span>
-
-  
-
-  
-  
-  </div>
-  <div class="header-bar"></div>
-
-  
-
-<div id="menubar-container" class="container">
-  <div id="menubar">
-    <div id="menus" class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-          <p  class="navbar-text indicator_area">
-          <span id="current-mode" >current mode</span>
-          </p>
-        <button type="button" class="btn btn-default navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <i class="fa fa-bars"></i>
-          <span class="navbar-text">Menu</span>
-        </button>
-        <ul class="nav navbar-nav navbar-right">
-          <li id="notification_area"></li>
-        </ul>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
-              <ul id="file-menu" class="dropdown-menu">
-                <li id="new-file"><a href="#">New</a></li>
-                <li id="save-file"><a href="#">Save</a></li>
-                <li id="rename-file"><a href="#">Rename</a></li>
-                <li id="download-file"><a href="#">Download</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</a>
-              <ul id="edit-menu" class="dropdown-menu">
-                <li id="menu-find"><a href="#">Find</a></li>
-                <li id="menu-replace"><a href="#">Find &amp; Replace</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Key Map</li>
-                <li id="menu-keymap-default"><a href="#">Default<i class="fa"></i></a></li>
-                <li id="menu-keymap-sublime"><a href="#">Sublime Text<i class="fa"></i></a></li>
-                <li id="menu-keymap-vim"><a href="#">Vim<i class="fa"></i></a></li>
-                <li id="menu-keymap-emacs"><a href="#">emacs<i class="fa"></i></a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">View</a>
-              <ul id="view-menu" class="dropdown-menu">
-              <li id="toggle_header" title="Show/Hide the logo and notebook title (above menu bar)">
-              <a href="#">Toggle Header</a></li>
-              <li id="menu-line-numbers"><a href="#">Toggle Line Numbers</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
-              <ul id="mode-menu" class="dropdown-menu">
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="lower-header-bar"></div>
+    def consultation(self, appointment):
+        """Confirms that a Healthcare Professional class conducted a specified Appointment object."""
+        # Print operation serves as a placeholder since consultation business process is unknown.
+        print(self.name, " conducted the consultation for ", appointment.patient, "'s ", appointment.appointment_type,
+              "appointment.")
 
 
-</div>
+class Doctor(HealthcareProfessional):
+    """Creates a Doctor object, inherits from Healthcare Professional class."""
+    def __init__(self, name, employee_number):
+        super().__init__(name, employee_number)  # Required to add another init attribute to Doctor subclass
+        self.registered_patient_list = []
 
-<div id="site">
+    def issue_prescription(self, patient, prescription_type, quantity, dosage):
+        """Returns a new Prescription object."""
+        new_prescription_type = prescription_type
+        new_patient = patient
+        new_doctor = self
+        new_quantity = quantity
+        new_dosage = dosage
+        return Prescription(new_prescription_type, new_patient, new_doctor, new_quantity, new_dosage)
+
+    def register_patient(self, patient): # Included to address the multiplicity requirement (0..500).
+        """Registers a Patient object to a Doctor; maximum of 500 Patients for each Doctor object."""
+        maximum_patients = 500
+        if len(self.registered_patient_list) < maximum_patients:
+            self.registered_patient_list.append(patient)
+        else:
+            print('Maximum number of registered patients exceeded.')
 
 
-<div id="texteditor-backdrop">
-<div id="texteditor-container" class="container"></div>
-</div>
+class Nurse(HealthcareProfessional):
+    """Creates a Nurse object, inherits from the Healthcare Professional class."""
+    pass
 
 
-</div>
+class Prescription:
+    """Creates a Prescription object, aggregating from Patient and Doctor objects."""
+    def __init__(self, prescription_type, patient, doctor, quantity, dosage):
+        self.prescription_type = prescription_type
+        self.patient = patient.name  # Refers to Patient objects
+        self.doctor = doctor.name  # Refers to Doctor objects
+        self.quantity = quantity
+        self.dosage = dosage
 
 
+class Appointment:
+    """Creates an Appointment object, aggregating from Patient and Healthcare Professional objects."""
+    def __init__(self, appointment_type, staff, patient):
+        self.appointment_type = appointment_type
+        self.staff = staff.name  # Refers to HealthcareProfessional objects
+        self.patient = patient.name  # Refers to Patient objects
 
 
+class AppointmentSchedule:
+    """Creates an Appointment Schedule object, composed of Appointment objects."""
+    def __init__(self):
+        # Instantiated sample appointment times and availability since surgery schedule is unknown.
+        # Dictionary data structure allows simple mechanism to assign objects and determine availability.
+        self.schedule = dict.fromkeys(['0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600'],
+                                      'Available')
+
+    def add_appointment(self, appointment, time):
+        """Adds an Appointment object to the Appointment Schedule at the specified time."""
+        # Tests if requested appointment time is valid and available
+        if time in self.schedule:
+            if self.schedule[time] == 'Available':
+                self.schedule[time] = appointment
+            else:
+                print('This appointment time is unavailable.')
+        else:
+            print('This is not a valid appointment time.')
+
+    def cancel_appointment(self, appointment):
+        """Removes all instances of an Appointment object from the Appointment Schedule."""
+        for time, appointment_scheduled in self.schedule.items():
+            if appointment_scheduled == appointment:
+                self.schedule[time] = 'Available'
+
+    def find_next_available(self):
+        """Returns the next available appointment time."""
+        if 'Available' in self.schedule.values():
+            for time, appointment_scheduled in sorted(self.schedule.items()):
+                if appointment_scheduled == 'Available':
+                    print('Next available appointment: ', time)
+                    break
+        else:
+            print('No appointments available.')
 
 
-    
+class Receptionist:
+    """Creates a Receptionist object with a name and employee number."""
+    def __init__(self, name, employee_number):
+        self.name = name
+        self.employee_number = employee_number
 
+    def make_appointment(self, appointment_type, staff, patient):
+        """Returns a new Appointment object."""
+        new_appointment_type = appointment_type
+        new_appointment_staff = staff
+        new_appointment_patient = patient
+        return Appointment(new_appointment_type, new_appointment_staff, new_appointment_patient)
 
-<script src="/static/edit/js/main.min.js?v=e3253dc1d2a1f445711555bd8ea58ada" type="text/javascript" charset="utf-8"></script>
-
-
-<script type='text/javascript'>
-  function _remove_token_from_url() {
-    if (window.location.search.length <= 1) {
-      return;
-    }
-    var search_parameters = window.location.search.slice(1).split('&');
-    for (var i = 0; i < search_parameters.length; i++) {
-      if (search_parameters[i].split('=')[0] === 'token') {
-        // remote token from search parameters
-        search_parameters.splice(i, 1);
-        var new_search = '';
-        if (search_parameters.length) {
-          new_search = '?' + search_parameters.join('&');
-        }
-        var new_url = window.location.origin + 
-                      window.location.pathname + 
-                      new_search + 
-                      window.location.hash;
-        window.history.replaceState({}, "", new_url);
-        return;
-      }
-    }
-  }
-  _remove_token_from_url();
-</script>
-</body>
-
-</html>
+    def cancel_appointment(self, appointment, schedule):
+        """Removes all instances of an Appointment object from a specified Appointment Schedule object."""
+        schedule.cancel_appointment(appointment)
+        return schedule
